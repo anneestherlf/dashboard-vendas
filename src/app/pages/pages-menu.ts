697344ccc828a1071f3478f1d93707e0e6cb1a81
Anne <102173382+anneestherlf@users.noreply.github.com>
2022-07-17 01:37:05 -0300

    first commit

---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,33 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'DASHBOARD - gráficos e análises',
+    group: true,
+  },
+  {
+    title: 'Vendas',
+    icon: 'layout-outline',
+    link: '/pages/dashboard',
+    home: true,
+    },
+    {
+      title: 'Produtos',
+      icon: 'layout-outline',
+      link: '',
+      home: true,
+      },
+      {
+        title: 'Clientes',
+        icon: 'layout-outline',
+        link: '',
+        home: true,
+        },
+        {
+          title: 'Fornecedores',
+          icon: 'layout-outline',
+          link: '',
+          home: true,
+          }
+
+]
