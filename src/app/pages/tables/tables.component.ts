46157d3040c7ec4f79563ef217d9f51fcdb448cc
Anne <102173382+anneestherlf@users.noreply.github.com>
2022-07-18 13:15:53 -0300

    cadastro de clientes

--- src/app/pages/tables/tables.component.ts
+++ src/app/pages/tables/tables.component.ts
@@ -1,8 +1,69 @@
 import { Component } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
+import { SmartTableData } from '../../@core/data/smart-table';
 
 @Component({
   selector: 'ngx-tables',
   templateUrl: './tables.component.html',
 })
 export class TablesComponent {
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      id: {
+        title: 'Cód',
+        type: 'number',
+      },
+      firstName: {
+        title: 'Nome',
+        type: 'string',
+      },
+      lastName: {
+        title: 'CPF',
+        type: 'number',
+      },
+      username: {
+        title: 'Endereço',
+        type: 'string',
+      },
+      email: {
+        title: 'E-mail',
+        type: 'string',
+      },
+      age: {
+        title: 'Telefone',
+        type: 'number',
+      },
+    },
+  };
+
+  source: LocalDataSource = new LocalDataSource();
+
+  constructor(private service: SmartTableData) {
+    const data = this.service.getData();
+    this.source.load(data);
+  }
+
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  }
 }
+
+
